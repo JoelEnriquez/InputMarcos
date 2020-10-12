c315ec98faacc1f6f7566762c374660e1eb86476
Joel Enriquez <jsem1331@gmail.com>
2020-10-11 23:46:09 -0600

    Servlet Add Medico

--- FormsProyectoMarcos/src/main/java/Agregar/AgregarMedico.java
+++ FormsProyectoMarcos/src/main/java/Agregar/AgregarMedico.java
@@ -8,6 +8,7 @@ package Agregar;
 import SQL.AuxInsertar;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Date;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -29,6 +30,16 @@ public class AgregarMedico extends HttpServlet {
         
         String codigo = auxInsertar.getCodigoMedicoInsertar();
         String nombre = request.getParameter("nombre");
+        int noColegiado = Integer.parseInt(request.getParameter("colegiado"));
+        String DPI = request.getParameter("DPI");
+        int noTelefono = Integer.parseInt(request.getParameter("telefono"));
+        String correo = request.getParameter("correo");
+        Date fechaInicio = Date.valueOf(request.getParameter("fecha_inicio"));
+        
+        String horaInicio = request.getParameter("hora_inicio");
+        String horaFin = request.getParameter("hora_fin");
+        
+        
         
         //Mandas a insertar el medico
       
