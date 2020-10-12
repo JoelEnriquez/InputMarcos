1a5a4c03acf6b3bcacb91fef62d8af6c98daeba5
Joel Enriquez <jsem1331@gmail.com>
2020-10-11 23:52:24 -0600

    Last commit

--- FormsProyectoMarcos/src/main/java/Agregar/AgregarMedico.java
+++ FormsProyectoMarcos/src/main/java/Agregar/AgregarMedico.java
@@ -9,6 +9,9 @@ import SQL.AuxInsertar;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Date;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -23,11 +26,13 @@ import javax.servlet.http.HttpServletResponse;
 public class AgregarMedico extends HttpServlet {
 
     private AuxInsertar auxInsertar = new AuxInsertar();
-     
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm");
+
         String codigo = auxInsertar.getCodigoMedicoInsertar();
         String nombre = request.getParameter("nombre");
         int noColegiado = Integer.parseInt(request.getParameter("colegiado"));
@@ -35,16 +40,23 @@ public class AgregarMedico extends HttpServlet {
         int noTelefono = Integer.parseInt(request.getParameter("telefono"));
         String correo = request.getParameter("correo");
         Date fechaInicio = Date.valueOf(request.getParameter("fecha_inicio"));
-        
-        String horaInicio = request.getParameter("hora_inicio");
-        String horaFin = request.getParameter("hora_fin");
-        
-        
-        
-        //Mandas a insertar el medico
-      
-    }
 
-    
+        String hInicio = request.getParameter("hora_inicio");
+        String hFin = request.getParameter("hora_fin");
+        Time horaInicio;
+        Time horaFin;
+
+        try {
+            java.util.Date dateAuxTimeInicio = (java.util.Date) format.parse(hInicio);
+            horaInicio = new Time(dateAuxTimeInicio.getTime());
+            
+            java.util.Date dateAuxTimeFin = (java.util.Date) format.parse(hFin);
+            horaFin = new Time(dateAuxTimeFin.getTime());
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+        }
+
+        //Mandas a insertar el medico
+    }
 
 }
